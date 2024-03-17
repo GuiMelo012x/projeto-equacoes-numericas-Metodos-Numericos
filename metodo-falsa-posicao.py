8a2431ef2176533a222570312b408f0a143e4d2b
Guilherme Melo <guilherme2020102296@unicap.br>
2024-03-17 13:05:27 -0300

    Método da Falsa-Posição - Comentando Códigos

--- metodo-falsa-posicao.py
+++ metodo-falsa-posicao.py
@@ -1,28 +1,31 @@
 def metodo_fp(func, a, b, erro_aceitavel):
     def f(x):
-        f = eval(func) # Lê a string dada pelo usuário e transforma em uma função matemática
+        f = eval(func)  # Lê a string dada pelo usuário e transforma em uma função matemática
         return f
 
-    i = 0
-    c_anterior = 0
-    c = (a * f(b) - b * f(a)) / (f(b) - f(a))
-    erro = abs(c-c_anterior)
+    i = 0  # O i será o contador de iterações.
+    c_anterior = 0  # valor de c da iteração anterior.
+    c = (a * f(b) - b * f(a)) / (f(b) - f(a))  # Calcula o primeiro valor de c.
+    erro = abs(c-c_anterior)  # Calcula o erro entre c e o c_anterior.
 
+   # Loop que irá girar até bater o número de iterações máximas.
     while erro > erro_aceitavel:
-        c_prox = (a * f(b) - b * f(a)) / (f(b) - f(a))
+        c_prox = (a * f(b) - b * f(a)) / (f(b) - f(a))  # Calcula o próximo valor de c
 
+        # Verifica se há raiz dentro do intervalo
         if f(a) * f(b) >= 0:
             print("Não há raiz.")
             quit()
 
+        # Atualiza o intervalo baseado no f(c_prox)
         elif f(c_prox) * f(a) < 0:
-            erro = abs(c_prox - b)
-            b = c_prox
+            erro = abs(c_prox - b)  # Atualiza o erro
+            b = c_prox  # Atualiza o limite superior do intervalo
             i = i + 1
 
         elif f(c_prox) * f(b) < 0:
-            erro = abs(c_prox - a)
-            a = c_prox
+            erro = abs(c_prox - a)  # Atualiza o erro
+            a = c_prox  # Atualiza o limite inferior do intervalo
             i = i + 1
 
 
@@ -32,6 +35,7 @@ def metodo_fp(func, a, b, erro_aceitavel):
 
 # Insira a sua função aqui:
 metodo_fp("(3*x ** 2) - 4",0,2,0.001)
+
 
 '''
 
@@ -44,6 +48,6 @@ c_prox --> calcula o próximo valor de 'c'.
 erro --> calcula o erro entre o c e o c_anterior. O erro é a diferença entre a estimativa atual da raiz e a estimativa anterior da raiz. Quando a diferença entre duas estimativas consecutivas da raiz for menor ou igual a erro_aceitavel, 
 significa que encontramos uma boa aproximação para a raiz.
 
-
 '''
+
 
